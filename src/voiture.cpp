a4400d84bbb03ffbd5bb4681c0632dfb2c9774da
Éric Dionne <ericd_420@hotmail.com>
2014-10-12 16:15:31 -0400

    commit correct

--- src/voiture.cpp
+++ src/voiture.cpp
@@ -21,7 +21,7 @@ Voiture::Voiture(std::string nomPhoto) : GameObject(sf::Sprite(),CAR)
     m_vitesse_courante = 0.0;
 
     petrole = 0;
-    money =0;
+    money =1000;
 
 
     GameObject::setTexture(TextureManager::getInstance().getTexture(nomPhoto));
@@ -35,8 +35,8 @@ Voiture::~Voiture()
 void Voiture::miseAJourVoiture()
 {
     m_vitesse_max = (m_vitesse_max_m+1 * 5);
-    m_acceleration = (m_acceleration_m  / 20 );
-    m_maniabilite = (m_maniabilite_m * 2 );
+    m_acceleration = ((m_acceleration_m+1) / 10);
+    m_maniabilite = (m_maniabilite_m+1 * 2 );
     m_penetrationZombie = (m_penetrationZombie_m);
 }
 
