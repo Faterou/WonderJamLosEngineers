c961f60b786c75d2ca3c3a42d31d52140796a2d8
Éric Dionne <ericd_420@hotmail.com>
2014-10-11 20:13:00 -0400

    ajout du menu

--- include/MenuScene.h
+++ include/MenuScene.h
@@ -1,7 +1,11 @@
 #ifndef MENUSCENE_H
 #define MENUSCENE_H
-
 #include <Scene.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <windows.h>
+
+#define MAX_NUMBER_OF_ITEMS 4
 
 
 class MenuScene : public Scene
@@ -9,12 +13,21 @@ class MenuScene : public Scene
     public:
         MenuScene();
         virtual ~MenuScene();
-
         virtual void inputs();
         virtual void update();
         virtual void draw();
+
+        void MoveUp();
+        void MoveDown();
+        int GetPressedItem() { return selectedItemIndex; }
+
     protected:
     private:
+        int selectedItemIndex = 0;
+        sf::Font font;
+
+        sf::Text menu[MAX_NUMBER_OF_ITEMS];
+
 };
 
 #endif // MENUSCENE_H
--- src/MenuScene.cpp
+++ src/MenuScene.cpp
@@ -1,4 +1,12 @@
 #include "MenuScene.h"
+#include "Map.h"
+#include "Destination.h"
+#include "StatsScene.h"
+#include "EndScene.h"
+#include "voiture.h"
+#include <stdlib.h>
+
+
 
 MenuScene::MenuScene()
 {
@@ -10,6 +18,101 @@ MenuScene::~MenuScene()
     //dtor
 }
 
-void MenuScene::inputs(){}
 void MenuScene::update(){}
-void MenuScene::draw(){}
+
+void MenuScene::inputs()
+{
+    sf::Event event1;
+
+    while (window.pollEvent(event1))
+    {
+        std::cout << selectedItemIndex << std::endl;
+
+        if (event1.type == sf::Event::KeyPressed)
+        {
+            std::cout << selectedItemIndex << std::endl;
+
+            if (event1.key.code == sf::Keyboard::Up)
+            {
+                if (selectedItemIndex - 1 >= 0)
+                {
+                menu[selectedItemIndex].setColor(sf::Color::White);
+                selectedItemIndex--;
+                menu[selectedItemIndex].setColor(sf::Color::Red);
+                }
+            }
+
+
+             if (event1.key.code == sf::Keyboard::Down)
+             {
+                std::cout << selectedItemIndex << std::endl;
+                if (selectedItemIndex + 1 < 4)
+                {
+                    std::cout << selectedItemIndex << std::endl;
+                    menu[selectedItemIndex].setColor(sf::Color::White);
+                    selectedItemIndex++;
+                    menu[selectedItemIndex].setColor(sf::Color::Red);
+                }
+            }
+        }
+
+    }
+
+}
+
+void MenuScene::draw()
+{
+    window.clear();
+
+    sf::Texture texture;
+    sf::Sprite sprite;
+    sf::Font font;
+    sf::Text menuPlay;
+    sf::Text menuExit;
+    sf::Text menuAide;
+    sf::Text menuSetting;
+
+    if (!texture.loadFromFile("imageMenu.png"))
+    {
+        std::cout << "Ca marche pas gros mongole" << std::endl;
+    }
+
+    sprite.setTexture(texture);
+
+    if (!font.loadFromFile("arial.ttf"))
+    {
+        std::cout << "erreur" << std::endl;
+    }
+
+    menu[0].setFont(font);
+    menu[0].setColor(sf::Color::Red);
+    menu[0].setString("Play");
+    menu[0].setPosition(sf::Vector2f(450, 125));
+
+    menu[1].setFont(font);
+    menu[1].setColor(sf::Color::White);
+    menu[1].setString("Help");
+    menu[1].setPosition(sf::Vector2f(450, 200));
+
+    menu[2].setFont(font);
+    menu[2].setColor(sf::Color::White);
+    menu[2].setString("Setting");
+    menu[2].setPosition(sf::Vector2f(425, 275));
+
+    menu[3].setFont(font);
+    menu[3].setColor(sf::Color::White);
+    menu[3].setString("Exit");
+    menu[3].setPosition(sf::Vector2f(450, 350));
+
+    window.draw(sprite);
+    for (int i = 0; i < 4; i++)
+    {
+        window.draw(menu[i]);
+    }
+
+    window.display();
+
+}
+
+
+
